0635f1db100ed1e1a749eb95e54959238651da43
fumi <fumilin01@gmail.com>
2018-09-20 23:59:00 +0900

    🔨 ログイン部分を別クラス(OngekiWebDriver)に分離

--- app.rb
+++ app.rb
@@ -1,26 +1,11 @@
-require 'selenium-webdriver'
-require 'dotenv'
+require './ongeki_web_driver'
 require './app_option'
 require './playlog'
 require './player_data'
 
 option = AppOption.new
 
-Dotenv.load
-
-options = Selenium::WebDriver::Chrome::Options.new
-options.add_argument('--headless')
-driver = Selenium::WebDriver.for :chrome, options: options
-
-driver.navigate.to 'https://ongeki-net.com/ongeki-mobile/'
-
-# ログイン
-driver.find_element(:name, 'segaId').send_keys(ENV['ONGEKI_SEGA_ID'])
-driver.find_element(:name, 'password').send_keys(ENV['ONGEKI_PASSWORD'])
-driver.find_element(:xpath, '//button[contains(@class, "btn_login_block")]').click
-
-# Aime選択 f_0は一番上のやつ
-driver.find_element(:xpath, '//button[contains(@class, "f_0")]').click
+driver = OngekiWebDriver.new.login
 
 # レコードのプレイ履歴収集
 playlog = Playlog.new
